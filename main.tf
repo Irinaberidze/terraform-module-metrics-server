0c6dd7f8cfd3296409b8c17e591528b56170e9e1
irinaberidze <irinaberidze18@gmail.com>
2023-02-28 16:25:08 -0600

    repo

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
-resource "helm_release" "metrics-server" {
-  name       = var.deployment_name
-  namespace  = var.namespace
-  repository = var.repository
-  chart      = var.chart
-  wait       = false
+module metrics-server {
+    source = "./module" 
+    deployment_name = "metrics-server"
+    namespace  = "kube-system"
+    repository = "https://prometheus-community.github.io/helm-charts"
+    chart      = "kube-state-metrics" 
 }
+
 
--- terraform.auto.tfvars
+++
@@ -1,4 +0,0 @@
-deployment_name = "metrics-server"
-namespace  = "kube-system"
-repository = "https://prometheus-community.github.io/helm-charts"
-chart      = "kube-state-metrics"
